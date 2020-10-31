53af4aad043241bfe3552170d076908c69c158e6
Razvan Rujoiu <rjrazvan@icloud.com>
2020-10-31 22:38:38 +0200

    updated gradle, added view binding

--- app/src/main/java/com/example/rentmanager/MainActivity.java
+++ app/src/main/java/com/example/rentmanager/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.rentmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.rentmanager.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        
+
     }
 }
