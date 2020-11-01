74b9cbc47011aa090cf43b106ab32db782d7394a
Razvan Rujoiu <rjrazvan@icloud.com>
2020-11-01 23:50:55 +0200

    added add user activity

---
+++ app/src/main/java/com/example/rentmanager/activities/AddUserActivity.java
@@ -0,0 +1,17 @@
+package com.example.rentmanager.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rentmanager.databinding.ActivityAddResidenceBinding;
+
+public class AddUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityAddResidenceBinding binding = ActivityAddResidenceBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
