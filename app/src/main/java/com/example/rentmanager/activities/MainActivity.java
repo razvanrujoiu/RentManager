b5dc3b45029d4e730150d06199dda531330d0581
Razvan Rujoiu <rjrazvan@icloud.com>
2020-11-07 15:25:31 +0200

    added navigation fragments

--- app/src/main/java/com/example/rentmanager/activities/MainActivity.java
+++ app/src/main/java/com/example/rentmanager/activities/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.rentmanager.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +11,10 @@ import android.view.MenuItem;
 
 import com.example.rentmanager.R;
 import com.example.rentmanager.databinding.ActivityMainBinding;
+import com.example.rentmanager.fragments.HomeFragment;
+import com.example.rentmanager.fragments.ResidencesFragment;
+import com.example.rentmanager.fragments.SettingsFragment;
+import com.example.rentmanager.models.Residence;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,16 +28,27 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = binding.bottomNavigation;
 
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
             switch (item.getItemId()) {
                 case R.id.action_home:
+                    selectedFragment = HomeFragment.newInstance();
                     break;
                 case R.id.action_residence:
+                    selectedFragment = ResidencesFragment.newInstance();
                     break;
                 case R.id.action_settings:
+                    selectedFragment = SettingsFragment.newInstance();
                     break;
             }
-            return false;
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame_layout, selectedFragment);
+            transaction.commit();
+            return true;
         });
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, HomeFragment.newInstance());
+        transaction.commit();
     }
 
     private void navigateToAddAddressActivity() {
